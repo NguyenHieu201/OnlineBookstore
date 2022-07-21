1539c25bb6d13f549a4e5a301f7799b1a0f38656
Nguyenkien736 <72335258+Nguyenkien736@users.noreply.github.com>
2022-07-21 21:32:15 +0700

    Create search.php

---
+++ application/views/home/search.php
@@ -0,0 +1,99 @@
+<body>
+        <div>
+            <div class="topbar">
+                <input placeholder="Search...">
+
+            </div>
+            <div class="prompt">
+                Results:
+            </div>
+            <div class="bodyitem">
+                <div class="navsidebar">
+                  <div class="labeltg">Tac gia</div>  
+                  <div class="tglist">
+                    <ul>
+                        <a href="#"><li>Lev tolstoise</li></a>
+                        <a href="#"><li>Shinkat</li></a>
+                        <a href="#"><li>Nguyen Nhat anh</li></a>
+                        <a href="#"><li>Thomas Rhett</li></a>
+                    </ul>
+                </div>
+                </div>
+            <div class="itemsgrid">
+                <div class="item">
+                    <div class="img" style="background-image: url('img_avatar3.png');">
+
+                    </div>
+                    <div class="tittle">
+                        Your name
+
+                    </div>
+                    <div class="price">37 000 d</div>
+
+                </div>
+                <div class="item">
+                    <div class="img" style="background-image: url('img_avatar3.png');">
+
+                    </div>
+                    <div class="tittle">
+                        Your name
+
+                    </div>
+                    <div class="price">37 000 d</div>
+
+                </div>
+                <div class="item">
+                    <div class="img" style="background-image: url('img_avatar3.png');">
+
+                    </div>
+                    <div class="tittle">
+                        Your name
+
+                    </div>
+                    <div class="price">37 000 d</div>
+
+                </div>
+                <div class="item">
+                    <div class="img" style="background-image: url('img_avatar3.png');">
+
+                    </div>
+                    <div class="tittle">
+                        Your name
+
+                    </div>
+                    <div class="price">37 000 d</div>
+
+                </div>
+                <div class="item">
+                    <div class="img" style="background-image: url('img_avatar3.png');">
+
+                    </div>
+                    <div class="tittle">
+                        Your name
+
+                    </div>
+                    <div class="price">37 000 d</div>
+
+                </div>
+                
+            </div>
+        </div>
+            <div class="botnav">
+                <div class="pagination">
+                    <a href="#">&laquo;</a>
+                    <a href="#">1</a>
+                    <a href="#">2</a>
+                    <a href="#">3</a>
+                    <a href="#">4</a>
+                    <a href="#">5</a>
+                    <a href="#">6</a>
+                    <a href="#">&raquo;</a>
+                  </div>
+            </div>
+            <div class="botbar">
+
+            </div>
+          
+            
+        </div>
+    </body>
